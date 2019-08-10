d95688f15945f31ea2154e7cea8e01605e68662d
Manish <tiwari.manish409@gmail.com>
2019-08-10 13:58:28 +0530

    subtract commit

--- test_calc.py
+++ test_calc.py
@@ -2,11 +2,11 @@ import unittest
 import calc
 
 class Testcalc(unittest.TestCase):
-
     def test_add(self):
         self.assertEqual(calc.calc_add(10,5),15)
-    def test_div(self):
-        self.assertEqual(calc.calc_div(20,10),2)
+    def test_sub(self):
+        self.assertEqual(calc.calc_add(10,5),5)
+
 
 
 
